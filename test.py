6908f05fe8ac9521228b6cb4a5d095c67aaf48ae
Erick Perales <erickles@us.ibm.com>
2025-09-17 21:46:57 -0500

    chore: prune tracked files now covered by .gitignore

--- test.py
+++
@@ -1,5 +0,0 @@
-import os, time, pathlib
-p = pathlib.Path("prices/azure_compute_cache_eastus.json")
-old = time.time() - 30*24*3600
-os.utime(p, (old, old))
-print("Backdated cache mtime ->", time.ctime(p.stat().st_mtime))
